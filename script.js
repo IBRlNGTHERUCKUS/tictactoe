f0db0c7a5c5b29a820fe308963253ae6ee5095b6
Joseph C <josephalexcobian@gmail.com>
2023-11-18 00:27:47 -0800

    style scoreboard

--- script.js
+++ script.js
@@ -3,12 +3,36 @@ function makePlayer(playerName, markType) {
 }
 
 const Players = {
-    player1: makePlayer('player1', 'X'),
-    player2: makePlayer('player2', 'O'),
+    player1: makePlayer('Player 1', 'X'),
+    player2: makePlayer('Player 2', 'O'),
 }
 
+const ScoreBoard = (function() {
+    let player1Score = 0;
+    let player2Score = 0;
+    const getScore = (player)=> {
+        if (player === Players.player1) {
+            return player1Score;
+        }
+        else {
+            return player2Score;
+        }
+    }
+    const incrementScore = (player)=> {
+        if (player === Players.player1) {
+            player1Score++;
+        }
+        else {
+            player2Score++;
+        }
+
+    }
+    return {getScore, incrementScore};
+}
+) ();
+
 const GameBoard = (function() {
-    let boardArray = [
+    let boardArray = [      //private
         'X', 'X', 'O',
         null, 'X', 'X',
         'O', null, 'X'
@@ -17,6 +41,16 @@ const GameBoard = (function() {
     const getboardArray = ()=> {
         return boardArray;
     }
+
+    const isFull = ()=> {
+        for (let i=0; i<9; i++) {
+            if (!boardArray[i]) {
+                return false
+            }
+        }
+        return true;
+    }
+
     const resetArray = () => {
         //Not sure why GameBoard. is required ???
         boardArray = [
@@ -26,11 +60,9 @@ const GameBoard = (function() {
         ]
     };
     const markArray= (index)=> {
-        console.log(`Marking index: ${index}: ${boardArray[index]}`)
         boardArray[index]=gameLogic.getTurn().markType;
-        console.log(`Now index: ${index}: ${boardArray[index]}`)
     }
-    return {getboardArray, resetArray, markArray};
+    return {getboardArray, resetArray, markArray, isFull};
 }) ();
 
 const gameLogic = (function() {
@@ -54,38 +86,74 @@ const gameLogic = (function() {
                 boardArray[i*3] === boardArray[i*3+1] &&
                 boardArray[i*3] === boardArray[i*3+2] &&
                 boardArray[i*3] != null
-                ) return boardArray[i*3];
+                ) return true;
             // Check columns for win
             else if (
                 boardArray[i] === boardArray[i+3] &&
                 boardArray[i] === boardArray[i+6] &&
                 boardArray[i] != null
-                ) return boardArray[i];
+                ) return true;
         }
         // Check \ diagonal for win
         if (
             boardArray[0] === boardArray[4] &&
             boardArray[0] === boardArray[8] &&
             boardArray[0] != null
-        ) return boardArray[0];
+        ) return true;
         // Check / diagonal for win
         else if (
             boardArray[2] === boardArray[4] &&
             boardArray[2] === boardArray[6] &&
             boardArray[2] != null
-            ) return boardArray[2];
+            ) return true;
     }
 
     return {getTurn, toggleTurn, checkWin};
 }) ();
 
-console.log(gameLogic.checkWin());
+const boardCellElements = document.querySelectorAll('.board-grid');
+const p1ScoreElement = document.querySelector('.player1');
+const p2ScoreElement = document.querySelector('.player2');
+function update() {
+    let i = 0;
+    for (let element of boardCellElements) {
+        element.textContent=GameBoard.getboardArray()[i];
+        i++;
+    }
+    p1ScoreElement.textContent = `${Players.player1.playerName} 
+     (${Players.player1.markType}): ${ScoreBoard.getScore(Players.player1)}`;
+    p2ScoreElement.textContent = `${Players.player2.playerName} 
+    (${Players.player2.markType}): ${ScoreBoard.getScore(Players.player2)}`;
+}
 
-console.log(GameBoard.getboardArray());
 GameBoard.resetArray();
-console.log(GameBoard.getboardArray());
-GameBoard.markArray(4);
-GameBoard.markArray(5);
-GameBoard.markArray(3);
-console.log(GameBoard.getboardArray())
-console.log(gameLogic.checkWin());
+update();
+
+
+function handleCellClick(e) {
+    let index = e.target.dataset.index;
+    console.log(e.target.dataset.index)
+    const boardArray = GameBoard.getboardArray();
+    // if the spot is already taken do nothing
+    if (boardArray[index] != null) {
+        return;
+    }
+    else {
+        GameBoard.markArray(index);
+        //Check for a win
+        if (gameLogic.checkWin()) {
+            ScoreBoard.incrementScore(gameLogic.getTurn());
+            GameBoard.resetArray();
+        }
+        //Check for a draw
+        else if (GameBoard.isFull()) {
+            GameBoard.resetArray();
+        }
+        gameLogic.toggleTurn();
+        update();
+    }
+}
+
+for (let element of boardCellElements) {
+    element.addEventListener('click', handleCellClick)
+}
